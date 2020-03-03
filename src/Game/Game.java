f457c390fa04ba572ee10be417ce71307bdc62ca
Fatima Farah <fatima.farah@connect-group.com>
2020-03-03 11:02:51 +0000

    Changed class structures - attempting to pull variables from other classes using the answer1 method and extending the classes.

---
+++ src/Game/Game.java
@@ -0,0 +1,57 @@
+package src.Game;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Game extends Answers {
+
+  //to use variables from another class you have to create that class
+  //Answers answer1 = new Answers();
+//System.out.println(answer1.answerOne);
+
+
+  public static void main(String[] args) {
+
+    Answers answer1 = new Answers();
+    Question question1 = new Question();
+
+
+    System.out.println("WELCOME TO FATIMA'S TEXT BASED GAME");
+    System.out.println(question1.getScenarioOne);
+
+    if (userInput = answer1.answers(0)) {
+      System.out.println("You rush out of bed in order to identify the noise and leave your room!");
+    }
+    else if (userInput = answer1.answers(1)) {
+      System.out.println("You have died");
+    }
+
+    System.out.println("You're now outside your room and you see the looming shadow"  + getUserInput);
+
+    if (userInput = answer1.answers(2)) {
+      System.out.println("The shadow grabbed you and killed you");
+    }
+
+    else if (userInput = answer1.answers(3)){
+      System.out.println("You run the other way and come across a knife on a table");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+  }
+
+
