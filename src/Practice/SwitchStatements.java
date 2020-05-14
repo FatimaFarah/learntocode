48e9ac22741b399e418e7f13fa3ecdd3180b5dd0
Alex Porter <alex.porter@connect-group.com>
2020-05-14 17:57:08 +0100

    note on switches

--- src/Practice/SwitchStatements.java
+++ src/Practice/SwitchStatements.java
@@ -5,7 +5,8 @@ public class SwitchStatements {
 public SwitchStatements(){
 
     String course = "Biology";
-
+    //without a break statement switches carry on
+    //with the input Biology the bellow statement will run the biology case and the default
     switch (course) {
       case "Algebra":
         // Enroll in Algebra
